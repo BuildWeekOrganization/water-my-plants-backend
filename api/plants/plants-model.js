c2bea0f7975b7ece793a4676648b88905e878e29
chandapwes <67353485+chandapwes@users.noreply.github.com>
2021-08-24 17:58:47 -0600

    plants router set up

---
+++ api/plants/plants-model.js
@@ -0,0 +1,52 @@
+const db = require('../data/db-config')
+
+async function getAllPlants() {
+    let results = await db('plants')
+    return results
+}
+
+function getPlantById(plant_id) {
+    return db('plants as p')
+    .select('p.user_id', 'p.nickname', 'p.species', 'p.h2o_frequency', 'p.image')
+    .where('p.plant_id', plant_id).first()
+}
+
+async function getByNickname(filter) {
+    let results = await db('plants').where(filter)
+    return results
+}
+
+function getBySpecies(filter) {
+    return db('users').where(filter)
+}
+
+async function addPlant(plant) {
+    const [newPlantObject] = await db('plants').insert(plant, ['user_id', 'nickname', 'species', 'h20_frequency', 'image'])
+    return newPlantObject
+}
+
+async function updatePlant(plant_id, info) {
+    const [updatedPlant] = await db('plants')
+        .where('plant_id', plant_id)
+        .update(info, [
+            'user_id', 'nickname', 'species', 'h2o_frequency', 'image',
+        ])
+        return updatedPlant
+}
+
+async function deletePlant(plant_id) {
+    const deletedPlant = await db('plants').where('plant_id', plant_id)
+    await db('plants').where('plant_id', plant_id).del()
+    return deletedPlant
+}
+
+module.exports = {
+    getAllPlants,
+    getPlantById,
+    getByNickname,
+    getBySpecies,
+    addPlant,
+    updatePlant,
+    deletePlant
+    
+  };
--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ const helmet = require('helmet')
 const cors = require('cors')
 
 const authRouter = require('./auth/auth-router')
+const plantsRouter = require('./plants/plants-router')
 
 const server = express()
 
@@ -11,6 +12,7 @@ server.use(cors())
 server.use(express.json())
 
 server.use('/api/auth', authRouter)
+server.use('/api/plants', plantsRouter)
 
 server.use((err, req, res, next) => { // eslint-disable-line
   res.status(err.status || 500).json({
